ffe50adcaae8f9700f21686fe57ad7ed8dbe58df
Paul Schofield <paul.schofield@medassets.com>
2011-12-16 22:02:46 -0600

    Preparing to branch to upgrade to NService 3.0 beta 1.

--- src/MembershipBC/app/LifeMap.Membership.Rest/Handlers/LoginHandler.cs
+++ src/MembershipBC/app/LifeMap.Membership.Rest/Handlers/LoginHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using LifeMap.Membership.Commands;
+using LifeMap.Membership.Messages.Commands;
 using LifeMap.Membership.Rest.Resources;
 using LifeMap.Membership.ViewModels;
 using OpenRasta.Web;
@@ -15,17 +16,12 @@ namespace LifeMap.Membership.Rest.Handlers
         {
             return new Login(registrationId);
         }
-        public object Post(Guid registrationId, string userName, string password)
+
+        public object Post(Guid registrationId, Guid loginId)
         {
-            var command = new EnterLoginForRegistrationCommand(registrationId, userName, password);
+            var command = new SelectLoginForRegistrationCommand(registrationId, loginId);
             Global.Bus.Publish(command);
-            return new OperationResult.SeeOther { RedirectLocation = new RegistrationViewModel { Id = registrationId }.CreateUri() };
-        }
-        public object Post(Login login)
-        {
-            var command = new EnterLoginForRegistrationCommand(login.RegistrationId, login.UserName, login.Password);
-            Global.Bus.Publish(command);
-            return new OperationResult.SeeOther { RedirectLocation = new RegistrationViewModel { Id = login.RegistrationId }.CreateUri() };
+            return new OperationResult.SeeOther { RedirectLocation = new Registration { Id = registrationId }.CreateUri() };
         }
     }
 }
